6830a519ba4d605c104ea58b8550121473953065
bachdx2812 <bachdx.hut@gmail.com>
2024-05-03 15:02:09 +0700

    init project issues list

---
+++ src/components/projects/[id]/ProjectIssues.vue
@@ -0,0 +1,12 @@
+<template>Project Issues List</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import { useRoute, useRouter } from "vue-router";
+
+const route = useRoute();
+const router = useRouter();
+
+const projectId = route.params.id;
+console.log(projectId);
+</script>
